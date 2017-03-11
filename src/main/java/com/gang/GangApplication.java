be9bca8523a2d7c0d19e7ea458dc31ad299d9128
sjaqjwor <lsklsk45@naver.com>
2017-03-11 18:57:13 +0900

    delete

--- src/main/java/com/gang/GangApplication.java
+++ src/main/java/com/gang/GangApplication.java
@@ -2,6 +2,8 @@ package com.gang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
 
 @SpringBootApplication
 public class GangApplication {
