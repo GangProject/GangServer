afaeb5af452b480767607b8ee1b4735b7a5d3da0
sumusb@naver.com <sumusb@naver.com>
2017-03-31 12:48:35 +0900

    [2017.03.31] cors수정 && devtools 추가

--- src/main/java/com/gang/config/MyWebConfig.java
+++ src/main/java/com/gang/config/MyWebConfig.java
@@ -1,8 +1,8 @@
 package com.gang.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
 
 /**
  * Created by iljun on 2017-03-17.
@@ -10,4 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Configuration
 @EnableWebMvc
 public class MyWebConfig extends WebMvcConfigurationSupport {
+
+    @Bean//global로 적용하기 위해 Bean Tag로 적용
+    public WebMvcConfigurer corsConfigure(){
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");//개발중이기때문에 모든 url open
+            }
+        };
+    }
 }
